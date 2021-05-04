de7ee2fb2b41e5a5e1dc7dc8bc6c79d31910e11f
JulianGutierritos <50029902+JulianGutierritos@users.noreply.github.com>
2021-05-03 23:22:58 -0500

    Cambio de ip

--- app/src/main/java/co/edu/eci/ieti/android/network/RetrofitNetwork.java
+++ app/src/main/java/co/edu/eci/ieti/android/network/RetrofitNetwork.java
@@ -19,7 +19,7 @@ public class RetrofitNetwork
 
     private AuthService authService;
     private TaskService taskService;
-    private String BASE_URL = "http:/192.168.1.80:8080/";
+    private String BASE_URL = "http:/10.0.2.2:8080/";
 
     public RetrofitNetwork( final String token )
     {
